b3c75cf00a64bcfe6c19daea9170d9147e20cc82
juanes G <jgalvez98@gmail.com>
2023-07-19 21:06:47 -0400

    javaScript local storage retrieval function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,71 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// code to display the current date in the header of the page//
 $(function () {
 
   var currentDayEl=$("#currentDay")
   var currentDayTime=dayjs().format("dddd, MMMM D YYYY, HH:mm")
+  var saveBtn= $(".saveBtn")
 
   console.log(currentDayTime)
 
   currentDayEl.text(currentDayTime)
-
-  // military time //
+  // military time var //
   var currentHour= dayjs().hour()
 
-    console.log(currentHour)
+  console.log(currentHour)
 // 1.create loop to get all the time bloks ID's startting from 9am
  // 2. compare the i with the current hour
-  for (let i = 9; i < 14; i++) {
+  for (let i = 9; i < 18; i++) {
     var timeBlock=$("#hour-"+i)
+    var event= localStorage.getItem("hour-"+i)
+
+    console.log(event)
+
+    
   if(i===currentHour){
 
     timeBlock.addClass("present")
   }
-  else if(currentHour>i){
+  else if(currentHour > i){
     timeBlock.addClass("past")
     }
-
+    else{
+      timeBlock.addClass("future")
+    }
 
   }
   
-  // listener for click events on the save button to save the user input in local storage//
 
-  $("button").click(function () {
-    for (let i = 9; i < 18; i++) {
-      var txt = $("#" + i).val();
-      if (txt i== null) {
-        var data = {text: txt };
-        localStorage.setItem(i, JSON.stringify(data));
-    };
+// select  text area and compare the current block by comparing the id to the current hour//
+//$ ("textarea").each(function () {
+  //var textAreaNumber = parseInt($(this).attr("id"));
+
+ // if(currentHour > textAreaNumber) {
+   // $(this).addClass("past");
+ // } else if (currentHour < textAreaNumber) {
+  //  $(this).addClass("future");
+ // } else {
+  //  $(this).addClass("present");
+  //}
+//});
+        // print storage  data func//
+  for (let i = 0; i < localStorage.length; i++) {
+   var key = localStorage.key(1);
+   var value = JSON.parse(localStorage.getItem(key));
+   $("#" + key).val(value.text);   
   };
-});
+      // save event / function //
+  function saveEvent(event){
+    var currentButton=$(event.target)
+    var textArea= currentButton.siblings("textarea")
+    var parentId= currentButton.parent().attr("id")
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  alert(textArea.val() +" "+ parentId)
+
+    localStorage.setItem(parentId, textArea.val())
+
+  }
+
+  saveBtn.on("click", saveEvent)
+
+
 });
